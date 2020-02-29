aec21b444eb72715c64bba07304580b6aed7ec51
Viacheslav Kokkei <Viacheslav_Kokkei@epam.com>
2020-02-29 19:17:48 +0200

    Added GlassMapper and code generation for models

---
+++ src/Foundation/GlassMapper/code/Models/IGlassBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Sitecore.Globalization;
+
+namespace Hackathon.Foundation.GlassMapper.Models
+{
+    public interface IGlassBase
+    {
+        [SitecoreId]
+        Guid Id { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Language)]
+        Language Language { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Version)]
+        int Version { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.BaseTemplateIds)]
+        IEnumerable<Guid> BaseTemplateIds { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateName)]
+        string TemplateName { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateId)]
+        Guid TemplateId { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Name)]
+        string ItemName { get; set; }
+    }
+}
